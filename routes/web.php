56e2cedf98265f002249928666a41c3c5859e353
Akmall-rey <raihanakmal354@gmail.com>
2024-06-06 15:33:10 +0700

    revisi2

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home.blade.php');
 });
 
 Route::get('/home', function () {
-    return view('home');
+    return view('home.blade.php');
 });
 
 Route::get('/shop', function () {
-    return view('shop');
+    return view('shop.blade.php');
 });
 
 Route::get('/profile', function () {
-    return view('profile');
+    return view('profile.blade.php');
 });
 
 Route::get('/cart', function () {
-    return view('cart');
+    return view('cart.blade.php');
 });
 
 Route::get('/checkout', function () {
-    return view('checkout');
+    return view('checkout.blade.php');
 });
 
 Route::get('/login-register', function () {
-    return view('login_register');
+    return view('login_register.blade.php');
 });
